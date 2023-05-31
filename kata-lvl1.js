08329b76ebd2b716c259b1354ba3ffb384efc9ae
Manuela Perez <manuelaperezag@gmail.com>
2023-05-31 14:33:24 +0200

    finished

--- kata-lvl1.js
+++ kata-lvl1.js
@@ -2,28 +2,34 @@
 //Escribe una funcion que retorne la suma de los numeros que recibe
 function suma(a, b) {
 	//escribe el codigo aqui
+	return (a + b);
 }
 
 //Escribe una funcion que retorne la resta de los dos numeros que recibe
 function resta(a, b) {
 	//escribe el codigo aqui
+	return (a - b);
 }
 
 //Escribe una funcion que retorne un array vacio
 function array() {
 	//escribe el codigo aqui
-
+	let array = [];
+	return (array);
 }
 
 //Escribe una funcion que retorne un array con 3 elementos
 function fullArray () {
-
+	let array = ["hola", "adios", "buenas"];
+	return (array);
 }
 
 //Usa el metodo adecuado para anadir el parametro al principio del array
 function addName(word) {
 	const array = ["Sal", "Azucar"];
 	//escribe el codigo aqui
+	array.unshift(word);
+	return (array);
 
 }
 
@@ -31,31 +37,37 @@ function addName(word) {
 function addOtherName(word) {
 	const array = ["Sal", "Azucar"]
 	//escribe el codigo aqui
-
+	array.push(word);
+	return (array);
 }
 
 //Usa el metodo adecuado para retornar la largada del parametro
 function nameLength(word) {
 	//escribe el codigo aqui
+	let len = word.length;
+	return (len);
 }
 
 //Usa el metodo adecuado que retorne el array que llega por parametro sin el ultimo elemento
 function deleteElement(array) {
 	//escribe el codigo aqui
-
-
+	array.pop();
+	return (array);
 }
 
 
 //Usa el metodo adecuado para ordenar el array del parametro
 function orderArray(array) {
 	//escribe aqui tu codigo
-
+	array.sort();
+	return (array);
 }
 
 //Usa el metodo adecuado para devolver el array sin el primer y el segundo elemento
 function cutArray(array) {
-	
+	array.shift();
+	array.shift();
+	return (array);
 }
 
 
